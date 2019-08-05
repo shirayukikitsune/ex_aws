a7f62d9b7df439fc110d65f80322cad7bfb96300
Bruno Ferreira <shirayukikitsune@gmail.com>
2019-08-05 12:30:15 -0300

    Extract method for config value fetching

--- lib/kitsune/aws/config.ex
+++ lib/kitsune/aws/config.ex
@@ -1,16 +1,17 @@
 defmodule Kitsune.Aws.Config do
-  def get_secret_key() do
-    k = Application.get_env :kitsune_aws, :secret_key
-    if k == nil, do: System.fetch_env("AWS_SECRET_ACCESS_KEY") |> elem(1), else: k
-  end
+  def get_secret_key(), do: get_var :secret_key, "AWS_SECRET_ACCESS_KEY"
 
-  def get_access_key() do
-    k = Application.get_env :kitsune_aws, :access_key
-    if k == nil, do: System.fetch_env("AWS_ACCESS_KEY_ID") |> elem(1), else: k
-  end
+  def get_access_key(), do: get_var :access_key, "AWS_ACCESS_KEY_ID"
 
-  def get_default_region() do
-    k = Application.get_env :kitsune_aws, :default_region
-    if k == nil, do: System.fetch_env("AWS_DEFAULT_REGION") |> elem(1), else: k
+  def get_default_region(), do: get_var :default_region, "AWS_DEFAULT_REGION"
+
+  defp get_var(key, env) do
+    value = Application.get_env :kitsune_aws, key
+    if value == nil do
+      value = System.fetch_env(env)
+      if value != :error, do: value |> elem(1), else: nil
+    else
+      value
+    end
   end
 end
